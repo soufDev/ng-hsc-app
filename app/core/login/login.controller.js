3378dd514f258452b5daae48c10be6b35711281f
aziouiz zakaria <zaziouiz@pictet-technologies.con>
2017-09-18 23:41:01 +0200

    Added login page.

--- app/core/login/login.controller.js
+++ app/core/login/login.controller.js
@@ -1,21 +1,29 @@
 (function () {
     'use strict';
 
-    angular.module('app.login', [])
+    angular.module('app.login', ['ngFeathers'])
+        .config(function ($feathersProvider) {
+            $feathersProvider.setEndpoint('');
+            $feathersProvider.useSocket(false);
+        })
+        .controller('LoginController', LoginController);
 
-            .controller('LoginController', LoginController);
+    LoginController.$inject = ['$feathers'];
 
-    LoginController.$inject = [];
-
-    function LoginController() {
+    function LoginController($feathers) {
         var vm = this;
+        vm.formBean = {login : "haha"};
 
-        var date = new Date();
-        vm.contactForm = {date: date};
-        vm.update = update;
+        vm.authenticate = function () {
+            $feathers.authenticate({
+                //TODO : finish this.
+            }).then(function (result) {
+                console.log('Authenticated!', result)
+            }).catch(function (error) {
+                console.error('Error authenticating!', error)
+            });
+        };
 
-        function update(contact) {
-            vm.contactForm = angular.copy(contact);
-        }
-    }
+        vm.authenticate();
+    };
 })();
